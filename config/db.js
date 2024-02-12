051b0daa7e0eaf7cf575e646df7ebaee59d8934a
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-02-12 16:33:28 +0700

    fix: db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
-     console.log(process.env.MONGO_URI);
      mongoose.set('strictQuery',true);
      const conn = await mongoose.connect(process.env.MONGO_URI);
 
