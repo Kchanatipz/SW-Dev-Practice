5daa551b92f7d78c20ccfdedfdafa621b7ac7d5b
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-03-13 21:35:48 +0700

    fix: fix api for getAppointment

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -2,6 +2,10 @@ const express = require('express');
 const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
 const router = express.Router();
 const {protect,authorize} = require('../middleware/auth');
+const appointmentRouter = require('./appointments');
+
+// re-route into other resource routers
+router.use('/:idhospitalId/appointments/',appointmentRouter);
 
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital);
